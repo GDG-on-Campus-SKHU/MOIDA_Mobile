7f914cc8c29dee1613725b93948bae1645d71ae1
devpark435 <devpark435@gmail.com>
2023-01-10 02:22:53 +0900

    Feat(refresh) : PostList Page Pull to Refresh

--- lib/Screens/Post/PostList.dart
+++ lib/Screens/Post/PostList.dart
@@ -15,68 +15,82 @@ class PostList extends StatefulWidget {
 
 class _PostListState extends State<PostList> {
   @override
+  void initState() {
+    // TODO: implement initState
+    listPost();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            icon: Icon(Icons.person),
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: ((context) => MyPage())));
-            },
-          )
-        ],
-      ),
-      body: FutureBuilder(
-        future: listPost(),
-        builder: ((context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-                itemCount: snapshot.data!.content!.length,
-                itemBuilder: ((context, index) {
-                  var contId = snapshot.data!.content![index].id;
-                  var title = snapshot.data!.content![index].title;
-                  var type = snapshot.data!.content![index].type;
-                  var mainContext = snapshot.data!.content![index].context;
-                  return GestureDetector(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.9,
-                        height: 100,
-                        color: Colors.amber,
-                        child: Column(
-                          children: [
-                            Text('${contId}'),
-                            Text('${title}'),
-                            Text('${type}'),
-                            Text('${mainContext}')
-                          ],
+    return RefreshIndicator(
+      onRefresh: () async {
+        await Future.delayed(Duration(milliseconds: 1500));
+        setState(() {
+          listPost();
+        });
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(Icons.person),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: ((context) => MyPage())));
+              },
+            )
+          ],
+        ),
+        body: FutureBuilder(
+          future: listPost(),
+          builder: ((context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.content!.length,
+                  itemBuilder: ((context, index) {
+                    var contId = snapshot.data!.content![index].id;
+                    var title = snapshot.data!.content![index].title;
+                    var type = snapshot.data!.content![index].type;
+                    var mainContext = snapshot.data!.content![index].context;
+                    return GestureDetector(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          height: 100,
+                          color: Colors.amber,
+                          child: Column(
+                            children: [
+                              Text('${contId}'),
+                              Text('${title}'),
+                              Text('${type}'),
+                              Text('${mainContext}')
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: ((context) => Post(
-                                    id: contId,
-                                  ))));
-                    },
-                  );
-                }));
-          } else {
-            return Center(
-              child: Text('no has data'),
-            );
-          }
-        }),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => Post(
+                                      id: contId,
+                                    ))));
+                      },
+                    );
+                  }));
+            } else {
+              return Center(
+                child: Text('no has data'),
+              );
+            }
+          }),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: (() {
+          Navigator.push(
+              context, MaterialPageRoute(builder: ((context) => WritePost())));
+        })),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (() {
-        Navigator.push(
-            context, MaterialPageRoute(builder: ((context) => WritePost())));
-      })),
     );
   }
 }
